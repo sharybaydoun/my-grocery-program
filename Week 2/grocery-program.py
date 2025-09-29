9421c55a74c3a07a9420cf42e4d9d92427a38c38
sharybaydoun <Sharibaydoun60@gmail.com>
2025-09-29 20:49:17 +0300

    Add quantity support and milk discount rule to shopping cart

--- Week 2/grocery-program.py
+++ Week 2/grocery-program.py
@@ -4,22 +4,30 @@ groceries = {
     "milk": 3,
     "bread": 2
 }
-shopping_list = []
 
-answer = input("What do you want to buy? (type 'done' to finish): ")
+shopping_cart = {}
+answer = input("What do you want to buy and how many? (type 'done' to finish): ")
 while answer != "done":
-    if answer in groceries:
-        shopping_list.append(answer)
+    parts = answer.split()
+    if len(parts) == 2 and parts[0] in groceries and parts[1].isdigit():
+        item = parts[0]
+        quantity = int(parts[1])
+        shopping_cart[item] = shopping_cart.get(item, 0) + quantity
     else:
-        print("Sorry, we don’t have that item")
-    answer = input("What do you want to buy? (type 'done' to finish): ")
-        
+        print("Sorry, we don’t have that item or the format is wrong (use: item quantity)")
+    answer = input("What do you want to buy and how many? (type 'done' to finish): ")
+  
+    
 # Calculate total cost after shopping is done
 totalcost = 0
-for item in shopping_list:
-    totalcost += groceries[item]
+for item in shopping_cart:
+      totalcost += groceries[item] * shopping_cart[item]
+      if item == "milk" and shopping_cart[item] > 2:
+        totalcost -=1
 
-print("You bought:", shopping_list)
+
+
+print("You bought:", shopping_cart)
 print("Total:", totalcost)
 if totalcost > 10:
     print("You spent a lot!")
---
+++ multiplication.py
@@ -0,0 +1,7 @@
+x=int(input("Enter a number:"))
+
+count=1
+while(count<=10):
+    result=count*x
+    print(result)
+    count+=1
